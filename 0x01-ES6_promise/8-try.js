6908d6851b70b96e944b1712950ffdcfdc3be55b
dietcokechan <dietcokechan@gmail.com>
2024-04-24 21:37:24 +0200

    no promise

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,9 +1,7 @@
 export default function divideFunction(numerator, denominator) {
-  return new Promise((resolve, reject) => {
-    if (denominator === 0) {
-      reject(new Error('Division by zero'));
-    } else {
-      resolve(numerator / denominator);
-    }
-  });
+  if (denominator === 0) {
+    throw Error('Division by zero');
+  } else {
+    return numerator / denominator;
+  }
 }
