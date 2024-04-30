a3b9c2d463756b25a0900a53761234ff6d2a4935
dietcokechan <dietcokechan@gmail.com>
2024-05-01 01:40:47 +0300

    revert

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -2,10 +2,11 @@ export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
   if (weakMap.has(endpoint)) {
-    if (weakMap.get(endpoint) >= 5) {
-      throw new Error("Endpoint load is high");
+    const count = weakMap.get(endpoint) || 0;
+    if (count >= 5) {
+      throw new Error('Endpoint load is high');
     }
-    weakMap.set(endpoint, (weakMap.get(endpoint) + 1));
+    weakMap.set(endpoint, count + 1);
   } else {
     weakMap.set(endpoint, 1);
   }
