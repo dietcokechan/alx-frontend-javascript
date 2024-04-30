45e631e3dde0b31106b3988f596076346d6358f3
dietcokechan <dietcokechan@gmail.com>
2024-04-30 17:27:12 +0300

    combine

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,9 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students.filter((student) => student.location === city).map((student) => {
+    const grade = newGrades.filter((newGrade) => newGrade.studentId === student.id)[0];
+    return {
+      ...student,
+      grade: grade ? grade.grade : 'N/A',
+    };
+  });
+}
