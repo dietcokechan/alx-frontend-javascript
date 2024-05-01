639a3abb6b2f6d24207b6699b844b868a300d766
dietcokechan <dietcokechan@gmail.com>
2024-05-01 17:10:32 +0300

    update

---
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -0,0 +1,18 @@
+export const cpp = new Subjects.Cpp();
+export const java = new Subjects.Java();
+export const react = new Subjects.React();
+
+export const cTeacher = {
+  firstName: "Guillaume",
+  lastName: "Salva",
+  experienceTeachingC: 10,
+};
+
+cpp.setTeacher(cTeacher);
+console.log(`C++ ${cpp.getRequirements()} ${cpp.getAvailableTeacher()}`);
+
+java.setTeacher(cTeacher);
+console.log(`Java ${java.getRequirements()} ${java.getAvailableTeacher()}`);
+
+react.setTeacher(cTeacher);
+console.log(`React ${react.getRequirements()} ${react.getAvailableTeacher()}`);
