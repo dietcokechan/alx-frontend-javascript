610c67760f213bb3f6eb120aa540356603436649
dietcokechan <dietcokechan@gmail.com>
2024-05-01 16:20:08 +0300

    advanced types p1

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,31 @@
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+class Director implements DirectorInterface {
+  workFromHome = (): string => "Working from home";
+  getCoffeeBreak = (): string => "Getting a coffee break";
+  workDirectorTasks = (): string => "Getting director tasks";
+}
+
+class Teacher implements TeacherInterface {
+  workFromHome = (): string => "Cannot work from home";
+  getCoffeeBreak = (): string => "Cannot have a break";
+  workTeacherTasks = (): string => "Getting to work";
+}
+
+const createEmployee = (salary: number | string): Director | Teacher => {
+  if (typeof salary === "number" && salary < 500) {
+    return new Teacher();
+  } else {
+    return new Director();
+  }
+};
