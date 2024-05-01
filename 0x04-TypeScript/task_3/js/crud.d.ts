45d8bad793e8b25823bd613091385d50f1ff7e66
dietcokechan <dietcokechan@gmail.com>
2024-05-01 16:38:13 +0300

    ambient namespaces

---
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -0,0 +1,7 @@
+import { RowID, RowElement } from "./interface";
+
+declare function insertRow(row: RowElement): number;
+declare function deleteRow(rowId: RowID): undefined;
+declare function updateRow(rowId: RowID, row: RowElement): number;
+
+export default { insertRow, deleteRow, updateRow };
