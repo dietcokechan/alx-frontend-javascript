ed11245acef97bcbed2cd24f034885e7719762f3
dietcokechan <dietcokechan@gmail.com>
2024-05-01 16:10:54 +0300

    writing a class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -13,4 +13,25 @@ interface Directors extends Teacher {
 
 const printTeacher = (firstName: string, lastName: string): string => {
   return `${firstName[0]}. ${lastName}`;
+};
+
+class StudentClass {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+interface StudentConstructor {
+    (firstName: string, lastName: string): StudentClass;
 }
